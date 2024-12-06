5eba59f84bf8ae4ccec8ab374c2f9c3a7e90ad77
Chae Noah <canoa4943@gmail.com>
2024-12-06 19:30:02 +0900

    init

---
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/HexaShoppingmallServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.hexashoppingmallservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaShoppingmallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexaShoppingmallServiceApplication.class, args);
+    }
+
+}
