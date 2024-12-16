1e696b1a21ac4a4178992fe4211843b1fe3f425c
iooah <iooah@naver.com>
2024-12-16 14:47:49 +0900

    feat: Book @Builder of() 추가

--- src/main/java/com/nhnacademy/hexashoppingmallservice/entity/book/Book.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/entity/book/Book.java
@@ -3,7 +3,6 @@ package com.nhnacademy.hexashoppingmallservice.entity.book;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -16,25 +15,30 @@ public class Book {
     private Long bookId;
 
     @Column(nullable = false,length = 100)
+    @Setter
     private String bookTitle;
 
     @Column(nullable = false, columnDefinition = "TEXT") // 명시적으로 mysql의 text로 지정
     @Lob // 대량 데이터를 저장할 수 있는 필드로 지정
+    @Setter
     private String bookDescription;
 
     @Column(nullable = false)
     private LocalDate bookPubDate;
 
     @Column(nullable = false)
+    @Setter
     private Long bookISBN;
 
     @Column(nullable = false)
     private int bookOriginPrice;
 
     @Column(nullable = false)
+    @Setter
     private int bookPrice;
 
     @Column(nullable = false)
+    @Setter
     private boolean bookWrappable = false;
 
     @Column(nullable = false , columnDefinition = "INT DEFAULT 0")
@@ -53,5 +57,54 @@ public class Book {
     @ManyToOne
     @JoinColumn(name = "book_status_id")
     private BookStatus bookStatus;
+
+    @Builder
+    private Book(String bookTitle,
+                 String bookDescription,
+                 LocalDate bookPubDate,
+                 Long bookISBN,
+                 int bookOriginPrice,
+                 int bookPrice,
+                 boolean bookWrappable,
+                 Publisher publisher,
+                 BookStatus bookStatus) {
+        this.bookTitle = bookTitle;
+        this.bookDescription = bookDescription;
+        this.bookPubDate = bookPubDate;
+        this.bookISBN = bookISBN;
+        this.bookOriginPrice = bookOriginPrice;
+        this.bookPrice = bookPrice;
+        this.bookWrappable = bookWrappable;
+        this.publisher = publisher;
+        this.bookStatus = bookStatus;
+
+        // 초기 값 설정
+        this.bookView = 0;
+        this.bookAmount = 0;
+        this.bookSellCount = 0L;
+    }
+
+
+    public static Book of(String bookTitle,
+                          String bookDescription,
+                          LocalDate bookPubDate,
+                          Long bookISBN,
+                          int bookOriginPrice,
+                          int bookPrice,
+                          boolean bookWrappable,
+                          Publisher publisher,
+                          BookStatus bookStatus) {
+        return Book.builder()
+                .bookTitle(bookTitle)
+                .bookDescription(bookDescription)
+                .bookPubDate(bookPubDate)
+                .bookISBN(bookISBN)
+                .bookOriginPrice(bookOriginPrice)
+                .bookPrice(bookPrice)
+                .bookWrappable(bookWrappable)
+                .publisher(publisher)
+                .bookStatus(bookStatus)
+                .build();
+    }
 
 }
