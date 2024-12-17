df2cf098418566682efb5557ccfd30d9832b180c
Chae Noah <canoa4943@gmail.com>
2024-12-17 10:29:41 +0900

    feat: 엔티티 추가

---
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/entity/order/Order.java
@@ -0,0 +1,42 @@
+package com.nhnacademy.hexashoppingmallservice.entity.order;
+
+import com.google.common.primitives.UnsignedInteger;
+import com.nhnacademy.hexashoppingmallservice.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = true)
+    private Member member;
+
+    @Column(nullable = false)
+    private Integer orderPrice;
+
+    @Column(nullable = false)
+    private LocalDateTime orderedAt;
+
+    @Column(nullable = false)
+    @Length(max = 5)
+    private String zoneCode;
+
+    @Column(nullable = false)
+    @Length(max = 200)
+    private String address;
+
+    @Column
+    @Length(max = 100)
+    private String addressDetail;
+}
