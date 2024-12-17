9b4ec715b70374ae4f46f200061f80c78b0796d4
iooah <iooah@naver.com>
2024-12-17 17:30:11 +0900

    fix: ISBN 중복확인 메서드 추가

--- src/main/java/com/nhnacademy/hexashoppingmallservice/repository/book/BookRepository.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/repository/book/BookRepository.java
@@ -70,4 +70,7 @@ public interface BookRepository extends JpaRepository<Book,Long> {
 
     // 도서 목록 - 최신순(출간일 기준)
     Page<Book> findBooksByBookPubDate(Pageable pageable);
+
+    // ISBN 중복확인
+    boolean existsByBookIsbn(@Param("bookIsbn") Long bookIsbn);
 }
