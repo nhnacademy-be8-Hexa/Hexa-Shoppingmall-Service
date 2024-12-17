8e0e6472a0b75fde94ac95629f06176f89b36fc7
iooah <iooah@naver.com>
2024-12-17 13:40:13 +0900

    feat: PublisherRepository 추가

---
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/repository/book/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.hexashoppingmallservice.repository.book;
+
+import com.nhnacademy.hexashoppingmallservice.entity.book.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Publisher,Long> {
+}
