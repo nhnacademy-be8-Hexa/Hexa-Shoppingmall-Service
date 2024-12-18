f9c6efc2fc88b3991b58501740faab91d489d322
iooah <iooah@naver.com>
2024-12-18 17:35:39 +0900

    feat: 도서 레저지토리 테스트 생성 (아직 구현안함)

---
+++ src/test/java/com/nhnacademy/hexashoppingmallservice/repository/book/BookRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.nhnacademy.hexashoppingmallservice.repository.book;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookRepositoryTest {
+
+    @Test
+    void findBooksByPublisherName() {
+    }
+
+    @Test
+    void findByBookTitleLike() {
+    }
+
+    @Test
+    void findBooksByAuthorNameLike() {
+    }
+
+    @Test
+    void findByOrderByBookViewDesc() {
+    }
+
+    @Test
+    void findByOrderByBookSellCount() {
+    }
+
+    @Test
+    void findBooksByCategoryIds() {
+    }
+
+    @Test
+    void findBooksOrderByLikeCountDesc() {
+    }
+
+    @Test
+    void findBooksByTagName() {
+    }
+
+    @Test
+    void findAllByOrderByBookPubDateDesc() {
+    }
+
+    @Test
+    void existsByBookIsbn() {
+    }
+}
