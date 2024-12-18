77163d67c3bf8600f094218e31d5dd8bb7acd1fe
iooah <iooah@naver.com>
2024-12-18 18:47:32 +0900

    fix: @PostMapping 수정

--- src/main/java/com/nhnacademy/hexashoppingmallservice/controller/book/BookController.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/controller/book/BookController.java
@@ -5,7 +5,6 @@ import com.nhnacademy.hexashoppingmallservice.entity.book.Book;
 import com.nhnacademy.hexashoppingmallservice.service.book.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,20 +14,18 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class BookController {
-    private final Integer SIZE = 10;
     private final BookService bookService;
     // 전체 조회
     // 어쨋든 뭐시기 해야하니까 다 받아서 required = false 해서 요청 온 것만 포함해서 if문 해서 로직 추가하기
-    @GetMapping("/api/books/")
-    public List<Book> getBooks(@RequestParam(defaultValue = "0") int page, @RequestParam(required = false) String search){
-        Pageable pageable = PageRequest.of(page,SIZE);
+    @GetMapping("/api/books")
+    public List<Book> getBooks(Pageable pageable, @RequestParam(required = false) String search){
         if(search != null && !search.isEmpty()){
             return bookService.getBooksByBookTitle(search,pageable);
         }
         return bookService.getBooks(pageable);
     }
     // 생성
-    @GetMapping("/api/books")
+    @PostMapping("/api/books")
     public ResponseEntity<Book> createBook(@RequestBody @Valid BookRequestDTO bookRequestDTO){
         return ResponseEntity.status(201).body(bookService.createBook(bookRequestDTO));
     }
