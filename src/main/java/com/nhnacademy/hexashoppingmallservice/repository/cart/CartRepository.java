58c3b2160d35cbadda7c3ca60b4d0b67e35aa561
Chae Noah <canoa4943@gmail.com>
2024-12-18 18:00:43 +0900

    fix: advice 누락 수정

--- src/main/java/com/nhnacademy/hexashoppingmallservice/repository/cart/CartRepository.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/repository/cart/CartRepository.java
@@ -1,7 +1,12 @@
 package com.nhnacademy.hexashoppingmallservice.repository.cart;
 
 import com.nhnacademy.hexashoppingmallservice.entity.cart.Cart;
+import com.nhnacademy.hexashoppingmallservice.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findAllByMember(Member member);
+
 }
