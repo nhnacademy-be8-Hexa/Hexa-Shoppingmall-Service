1605d0c892baabb296418686341f9eba0382368f
noahchae <104444048+noah-cha@users.noreply.github.com>
2024-12-18 17:55:57 +0900

    Delete src/main/java/com/nhnacademy/hexashoppingmallservice/repository/Cart directory
    fix: 잘못 생성된 Cart 파일 삭제

--- src/main/java/com/nhnacademy/hexashoppingmallservice/repository/Cart/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.hexashoppingmallservice.repository.Cart;
-
-import com.nhnacademy.hexashoppingmallservice.entity.cart.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
