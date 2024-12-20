aa383601a43c2a5f7451034b8775667fe002675b
조승주 <tmdwn3318@gmail.com>
2024-12-20 15:24:17 +0900

    fix: 멤버 컨트롤러 /admin -> /auth

--- src/main/java/com/nhnacademy/hexashoppingmallservice/controller/member/MemberController.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/controller/member/MemberController.java
@@ -20,7 +20,7 @@ public class MemberController {
     private final Integer SIZE = 10;
     private final MemberService memberService;
 
-    @GetMapping("/api/admin/members")
+    @GetMapping("/api/auth/members")
     public List<MemberProjection> getMembers(
             @RequestParam(defaultValue = "0") int page, @RequestParam(required = false) String search) {
         Pageable pageable = PageRequest.of(page, SIZE);
