aab8946f331677860b719d4063d831b9981e2315
kyubin <kyubinlee22@gmail.com>
2024-12-20 15:59:23 +0900

    fix: elasticsearch 서비스 쿼리 로직 수정

--- src/main/java/com/nhnacademy/hexashoppingmallservice/controller/elasticsearch/ElasticSearchController.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/controller/elasticsearch/ElasticSearchController.java
@@ -6,51 +6,60 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/search")
 public class ElasticSearchController {
 
     @Autowired
     private ElasticSearchService elasticSearchService;
 
-//    @PostMapping
-//    public Book addBook(@RequestBody Book book) {
-//        return elasticSearchService.saveBook(book);
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return elasticSearchService.saveBook(book);
+    }
+
+    @GetMapping
+    public List<Book> searchBooks(@RequestParam("search") String search, Pageable pageable) {
+        return elasticSearchService.searchBooks(search, pageable);
+    }
+
+
+//    @GetMapping
+//    public List<Book> searchBooksByTitle(@RequestParam("title") String title, Pageable pageable) {
+//        return elasticSearchService.searchBooksByTitle(title, pageable);
 //    }
 
-    @GetMapping("/search/title")
-    public List<Book> searchBooksByTitle(@RequestParam("title") String title, Pageable pageable) {
-        return elasticSearchService.searchBooksByTitle(title, pageable);
-    }
+//    @GetMapping
+//    public List<Book> searchBooksByAuthor(@RequestParam("author") String author, Pageable pageable) {
+//        return elasticSearchService.searchBooksByAuthor(author, pageable);
+//    }
+//
+//    @GetMapping
+//    public List<Book> searchBooksByDescription(@RequestParam("description") String description, Pageable pageable) {
+//        return elasticSearchService.searchBooksByDescription(description, pageable);
+//    }
+//
+//    @GetMapping
+//    public List<Book> searchBooksByTag(@RequestParam("tag") String tag, Pageable pageable) {
+//        return elasticSearchService.searchBooksByTag(tag, pageable);
+//    }
+//
+//    @GetMapping
+//    public List<Book> searchBooksByIsbn(@RequestParam("isbn") String isbn) {
+//        return elasticSearchService.searchBooksByIsbn(isbn);
+//    }
+//
+//    @GetMapping
+//    public List<Book> searchBooksBySellCount(Pageable pageable) {
+//        return elasticSearchService.searchBooksBySellCount(pageable);
+//    }
 
-    @GetMapping("/search/author")
-    public List<Book> searchBooksByAuthor(@RequestParam("author") String author, Pageable pageable) {
-        return elasticSearchService.searchBooksByAuthor(author, pageable);
-    }
-
-    @GetMapping("/search/description")
-    public List<Book> searchBooksByDescription(@RequestParam("description") String description, Pageable pageable) {
-        return elasticSearchService.searchBooksByDescription(description, pageable);
-    }
-
-    @GetMapping("/search/tag")
-    public List<Book> searchBooksByTag(@RequestParam("tag") String tag, Pageable pageable) {
-        return elasticSearchService.searchBooksByTag(tag, pageable);
-    }
-
-    @GetMapping("/search/isbn")
-    public List<Book> searchBooksByIsbn(@RequestParam("isbn") String isbn) {
-        return elasticSearchService.searchBooksByIsbn(isbn);
-    }
-
-    @GetMapping("/search/sellCount")
-    public List<Book> searchBooksBySellCount(Pageable pageable) {
-        return elasticSearchService.searchBooksBySellCount(pageable);
-    }
 
 }
