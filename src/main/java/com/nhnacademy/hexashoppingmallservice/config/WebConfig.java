e8cd86aed669331e6df87ec113e648bc46956504
Que-Sera-Sera-Today <30decade@gmail.com>
2025-01-08 22:57:47 +0900

    feat: 게스트 주문 내역 조회 추가

--- src/main/java/com/nhnacademy/hexashoppingmallservice/config/WebConfig.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.nhnacademy.hexashoppingmallservice.config;
 
 import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CustomPageableResolver());
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
