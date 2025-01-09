33df1a6f70e3570351e63e69545dd430c5eefcfa
조승주 <tmdwn3318@gmail.com>
2025-01-09 09:32:41 +0900

    feat: toss payment 레포지, 서비스 구현
    저장과 조회만 하면 됨

---
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/repository/payment/TossPaymentRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.hexashoppingmallservice.repository.payment;
+
+import com.nhnacademy.hexashoppingmallservice.entity.payment.TossPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TossPaymentRepository extends JpaRepository<TossPayment, String> {
+    
+}
