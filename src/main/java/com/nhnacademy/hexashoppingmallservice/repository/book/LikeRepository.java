fa160c3c36cef6dff0c492f4fd68fa711a1b95b6
kyubin <kyubinlee22@gmail.com>
2025-01-09 14:38:47 +0900

    feat : 좋아요 서비스 상태 전환 기능 추가

--- src/main/java/com/nhnacademy/hexashoppingmallservice/repository/book/LikeRepository.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/repository/book/LikeRepository.java
@@ -2,17 +2,16 @@ package com.nhnacademy.hexashoppingmallservice.repository.book;
 
 import com.nhnacademy.hexashoppingmallservice.entity.book.Book;
 import com.nhnacademy.hexashoppingmallservice.entity.book.Like;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Long countByBookBookId(Long bookId);
+
     Boolean existsByBookBookIdAndMemberMemberId(Long bookId, String memberId);
+
     /**
      * 특정 멤버가 좋아요한 책 목록을 조회합니다.
      *
@@ -21,4 +20,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
      */
     @Query("SELECT l.book FROM Like l WHERE l.member.memberId = :memberId")
     List<Book> findBooksLikedByMemberId(@Param("memberId") String memberId);
+
+    void deleteByBookBookIdAndMemberMemberId(Long bookId, String memberId);
 }
