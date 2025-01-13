cbabc4b2d8b2af858452367f8a27dba85bb019cd
highsiverr <ye90202@gmail.com>
2025-01-13 17:18:45 +0900

    feat: 사용자가 가지고 있는 쿠폰인지 검사하는 컨트롤러 추가

--- src/main/java/com/nhnacademy/hexashoppingmallservice/repository/member/MemberCouponRepository.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/repository/member/MemberCouponRepository.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.hexashoppingmallservice.repository.member;
 
+import com.nhnacademy.hexashoppingmallservice.entity.member.Member;
 import com.nhnacademy.hexashoppingmallservice.entity.member.MemberCoupon;
 import com.nhnacademy.hexashoppingmallservice.projection.member.MemberCouponProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,10 @@ import java.util.List;
 
 public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
     List<MemberCouponProjection> findByMemberMemberId(String memberId);
+
+    // 이미 발급받은 쿠폰인지 확인
+    boolean existsByCouponIdAndMember(Long couponId, Member member);
+
+    // 유저에게 나눠준 쿠폰인지 확인
+    boolean existsByCouponId(Long couponId);
 }
