558b9339cee999122932aadec997609cc04d1f5c
조승주 <tmdwn3318@gmail.com>
2025-01-17 22:58:41 +0900

    feat: pageable을 리퀘스트 파람으로 원래 받을 수 있으므로 커스텀 pageable resolver 제거

--- src/main/java/com/nhnacademy/hexashoppingmallservice/config/WebConfig.java
+++ src/main/java/com/nhnacademy/hexashoppingmallservice/config/WebConfig.java
@@ -11,10 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new CustomPageableResolver());
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
